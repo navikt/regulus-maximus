2bfbc12ba6e1a69ca21a348259359c0b22f891b7
Jørn-Are Flaten <ja.flaten91@gmail.com>
2024-02-08 14:50:23 +0100

    chore: fixed database connection
    Co-authored-by: Karl O <k@rl.run>
    Co-authored-by: Andreas <danduras@gmail.com>
    Co-authored-by: Natalie Uranes <natalie.uranes@gmail.com>
    Co-authored-by: Helene Arnesen <helene.arnesen@nav.no>

--- src/main/kotlin/no/nav/tsm/mottak/Environment.kt
+++ src/main/kotlin/no/nav/tsm/mottak/Environment.kt
@@ -10,25 +10,28 @@ class Environment(
 )
 
 object EnvironmentSingleton {
-  private var instance: Environment? = null
+    private var instance: Environment? = null
 
-  fun instance(config: ApplicationConfig): Environment {
-    if (instance != null) {
-      return instance ?: throw IllegalStateException("Environment not initialized")
-    }
+    fun instance(config: ApplicationConfig): Environment {
+        if (instance != null) {
+            return instance ?: throw IllegalStateException("Environment not initialized")
+        }
 
-    return Environment(
-            jdbcUrl = "jdbc:${config.requiredEnv("ktor.database.dbUrl")}",
+        val host = config.requiredEnv("ktor.database.dbHost")
+        val port = config.requiredEnv("ktor.database.dbPort")
+        val database = config.requiredEnv("ktor.database.dbName")
+        return Environment(
+            jdbcUrl = "jdbc:postgresql://$host:$port/$database",
             dbUser = config.requiredEnv("ktor.database.dbUser"),
             dbPassword = config.requiredEnv("ktor.database.dbPassword"),
         )
-        .also { instance = it }
-  }
+            .also { instance = it }
+    }
 
-  private fun ApplicationConfig.requiredEnv(name: String) =
-      propertyOrNull(name)?.getString()
-          ?: throw IllegalArgumentException("Missing required environment variable $name")
+    private fun ApplicationConfig.requiredEnv(name: String) =
+        propertyOrNull(name)?.getString()
+            ?: throw IllegalArgumentException("Missing required environment variable $name")
 }
 
 val ApplicationEnvironment.env: Environment
-  get() = EnvironmentSingleton.instance(this.config)
+    get() = EnvironmentSingleton.instance(this.config)
