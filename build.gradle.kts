ad51b8c8609cd918d1868b421f834124934d08b2
Helene Arnesen <helene.arnesen@nav.no>
2024-09-27 09:37:27 +0200

    chore: spring gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,67 +1,57 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val logback_encoder_version: String by project
 
-val prometheus_version: String by project
-val exposed_version: String by project
-val flyway_version: String by project
-val koin_version: String by project
-val postgres_version: String by project
-val jackson_version: String by project
 plugins {
-    kotlin("jvm") version "2.0.10"
-    id("io.ktor.plugin") version "2.3.12"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.10"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.8.0"
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "no.nav.tsm.mottak"
 version = "0.0.2"
 
-application {
-    mainClass.set("no.nav.tsm.mottak.ApplicationKt")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-}
-
 repositories {
     mavenCentral()
 }
 
+val ktor_version = "2.3.7"
+val kotlin_version = "1.9.22"
+val logback_version = "1.4.14"
+val logback_encoder_version = "7.2"
+val prometheus_version = "1.6.3"
+val exposed_version= "0.41.1"
+val flyway_version= "10.6.0"
+val postgres_version= "42.7.2"
+val koin_version= "3.5.3"
+val jackson_version= "2.16.1"
+
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-config-yaml")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
     implementation("io.micrometer:micrometer-registry-prometheus:$prometheus_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.flywaydb:flyway-database-postgresql:$flyway_version")
     implementation("org.postgresql:postgresql:$postgres_version")
-    implementation("io.ktor:ktor-server-netty-jvm")
     implementation("net.logstash.logback:logstash-logback-encoder:${logback_encoder_version}")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.insert-koin:koin-ktor:$koin_version")
-    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jackson_version")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jackson_version")
     implementation("org.apache.kafka:kafka-clients:3.8.0")
-    // Used for local dev tools only
-    implementation("io.ktor:ktor-server-html-builder:$ktor_version")
 
     compileOnly("org.flywaydb:flyway-core:$flyway_version")
-
-    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
 tasks {
-    shadowJar {
-        mergeServiceFiles {
-
-        }
+    bootJar {
+        archiveFileName = "app.jar"
     }
 }
