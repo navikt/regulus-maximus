1f813db1e3f093af2ecb3e40b3389c272a3740d2
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-10-03 09:34:58 +0200

    Adding publication to topic

--- src/main/kotlin/no/nav/tsm/mottak/sykmelding/kafka/SykmeldingConsumer.kt
+++ src/main/kotlin/no/nav/tsm/mottak/sykmelding/kafka/SykmeldingConsumer.kt
@@ -12,6 +12,7 @@ import no.nav.tsm.mottak.sykmelding.kafka.model.SykmeldingMedBehandlingsutfall
 import no.nav.tsm.mottak.sykmelding.kafka.util.SykmeldingModule
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
@@ -19,33 +20,34 @@ import org.springframework.stereotype.Service
 @Service
 class SykmeldingConsumer(
     private val kafkaConfigProperties: KafkaConfigProperties,
-    //private val kafkaTemplate: KafkaTemplate<String, SykmeldingMedUtfall>,
+    private val kafkaTemplate: KafkaTemplate<String, SykmeldingMedBehandlingsutfall>,
     private val sykmeldingService: SykmeldingService,
 ) {
     private val logger = LoggerFactory.getLogger(SykmeldingConsumer::class.java)
+
+    @Value("\${spring.kafka.topics.tsm-sykmelding}")
+    lateinit var tsmSykmeldingTopic: String
 
     @KafkaListener(topics = ["\${spring.kafka.topics.mottatt-sykmelding}"], groupId = "\${spring.kafka.group-id}")
     fun consume(cr: ConsumerRecord<String, String>?) {
         try {
             if (cr != null) {
                 val sykmelding = objectMapper.readValue(cr.value(), SykmeldingMedBehandlingsutfall::class.java)
+                // if (cr.headers())
                 logger.info("Received message from topic: ${cr.value()}")
                 runBlocking {
                     sykmeldingService.saveSykmelding(sykmelding)
                 }
-            }
 
+                kafkaTemplate.send(tsmSykmeldingTopic, SykmeldingMedBehandlingsutfall(sykmelding = sykmelding.sykmelding, validation = sykmelding.validation, kilde = sykmelding.kilde))
+            }
         } catch (e: Throwable) {
             logger.error("Kunne ikke lese melding fra topic ", e)
             throw e
         }
-
-        // her skal videre funksjonalitet ligge
-        /* try {
-             kafkaTemplate.send("tsm.sykmelding", SykmeldingMedUtfall(sykmeldingInput = sykmelding.sykmeldingInput, utfall = sykmelding.utfall))
-         } catch (ex: Exception) {
+          catch (ex: Exception) {
              logger.error("Failed to publish sykmelding to tsm.sykmelding", ex)
-         }*/
+         }
 
     }
 }
