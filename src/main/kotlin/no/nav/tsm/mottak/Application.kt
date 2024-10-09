af8bf21c21d85420179f9bfba23d216b99bba2ed
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-10-09 11:56:25 +0200

    Added kafka topics in application.yaml

--- src/main/kotlin/no/nav/tsm/mottak/Application.kt
+++ src/main/kotlin/no/nav/tsm/mottak/Application.kt
@@ -1,7 +1,6 @@
 package no.nav.tsm.mottak
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
