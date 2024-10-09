96c1cad75b2d39d60240068d4d64150bfbf705ca
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-10-09 13:53:12 +0200

    Added Kafka Consumer Config

---
+++ src/main/kotlin/no/nav/tsm/mottak/config/KafkaConsumerConfig.kt
@@ -0,0 +1,23 @@
+package no.nav.tsm.mottak.config
+
+import no.nav.tsm.mottak.sykmelding.kafka.model.SykmeldingMedBehandlingsutfall
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.support.serializer.JsonDeserializer.TRUSTED_PACKAGES
+import java.util.*
+
+@Configuration
+class KafkaConsumerConfig {
+
+    @Bean
+    fun containerFactory(props: KafkaProperties) =
+        ConcurrentKafkaListenerContainerFactory<UUID, SykmeldingMedBehandlingsutfall>().apply {
+            containerProperties.isObservationEnabled = true
+            consumerFactory = DefaultKafkaConsumerFactory(props.buildConsumerProperties(null).apply {
+                put(TRUSTED_PACKAGES, "no.nav.tsm.mottak.sykmelding.kafka.model")
+            })
+        }
+}
