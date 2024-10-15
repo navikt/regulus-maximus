852959438d8c04d095dd28b6048ed80b4733db1f
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-10-15 09:40:58 +0200

    Mapping to JSON objects instead of complex types

--- src/main/kotlin/no/nav/tsm/mottak/db/SykmeldingBehandlingsutfall.kt
+++ src/main/kotlin/no/nav/tsm/mottak/db/SykmeldingBehandlingsutfall.kt
@@ -1,7 +1,5 @@
 package no.nav.tsm.mottak.db
 import io.r2dbc.postgresql.codec.Json
-import no.nav.tsm.mottak.sykmelding.kafka.model.metadata.Meldingsinformasjon
-import no.nav.tsm.mottak.sykmelding.kafka.model.metadata.PersonId
 import org.springframework.data.relational.core.mapping.Table
 
 
@@ -11,12 +9,12 @@ import java.time.OffsetDateTime
 @Table("sykmelding_behandlingsutfall")
 data class SykmeldingBehandlingsutfall (
     val sykmeldingId: String,
-    val pasientIdent: List<PersonId>,
+    val pasientIdent: Json,
     val fom: LocalDate,
     val tom: LocalDate,
     val generatedDate: OffsetDateTime?,
     val sykmelding: Json,
     val metadata: Json,
     val validation: Json,
-    val meldingsinformasjon: Meldingsinformasjon
+    val meldingsinformasjon: Json
 )
--- src/main/kotlin/no/nav/tsm/mottak/db/SykmeldingMapper.kt
+++ src/main/kotlin/no/nav/tsm/mottak/db/SykmeldingMapper.kt
@@ -14,14 +14,14 @@ class SykmeldingMapper {
     ): SykmeldingBehandlingsutfall {
         return SykmeldingBehandlingsutfall(
             sykmeldingId = sykmeldingMedBehandlingsutfall.sykmelding.id,
-            pasientIdent = sykmeldingMedBehandlingsutfall.sykmelding.pasient.ids,
+            pasientIdent = Json.of(objectMapper.writeValueAsString(sykmeldingMedBehandlingsutfall.sykmelding.pasient.ids)),
             fom = sykmeldingMedBehandlingsutfall.sykmelding.aktivitet.first().fom,
             tom = sykmeldingMedBehandlingsutfall.sykmelding.aktivitet.last().tom,
             generatedDate = sykmeldingMedBehandlingsutfall.sykmelding.generatedDate,
             sykmelding = Json.of(objectMapper.writeValueAsString(sykmeldingMedBehandlingsutfall.sykmelding)),
             metadata = Json.of(objectMapper.writeValueAsString(sykmeldingMedBehandlingsutfall.sykmelding.metadata)),
             validation = Json.of(objectMapper.writeValueAsString(sykmeldingMedBehandlingsutfall.validation)),
-            meldingsinformasjon = sykmeldingMedBehandlingsutfall.meldingsInformasjon
+            meldingsinformasjon = Json.of(objectMapper.writeValueAsString(sykmeldingMedBehandlingsutfall.meldingsInformasjon)),
         )
     }
 }
