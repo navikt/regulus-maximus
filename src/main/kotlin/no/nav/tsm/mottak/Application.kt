58cf1d469668a74c770567b751d0184df6ade5eb
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-11-19 16:16:57 +0100

    Added autoconfiguration

--- src/main/kotlin/no/nav/tsm/mottak/Application.kt
+++ src/main/kotlin/no/nav/tsm/mottak/Application.kt
@@ -1,6 +1,7 @@
 package no.nav.tsm.mottak
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 import org.springframework.kafka.annotation.EnableKafka
@@ -8,6 +9,7 @@ import org.springframework.kafka.annotation.EnableKafka
 @SpringBootApplication
 @EnableKafka
 @EnableJdbcRepositories
+@ConfigurationPropertiesScan
 class Application
 
 fun main(args: Array<String>) {
