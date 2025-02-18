27242473181bfa2435df2601fd67b4823074fb81
Andreas Sagen Aspaas <danduras@gmail.com>
2025-02-18 12:27:20 +0100

    chore: remove manuellbehandling check

--- src/main/kotlin/no/nav/tsm/mottak/sykmelding/service/SykmeldingService.kt
+++ src/main/kotlin/no/nav/tsm/mottak/sykmelding/service/SykmeldingService.kt
@@ -2,7 +2,6 @@ package no.nav.tsm.mottak.sykmelding.service
 
 import no.nav.tsm.mottak.db.SykmeldingMapper
 import no.nav.tsm.mottak.db.SykmeldingRepository
-import no.nav.tsm.mottak.manuell.ManuellbehandlingService
 import no.nav.tsm.mottak.pdl.IDENT_GRUPPE
 import no.nav.tsm.mottak.pdl.PdlClient
 import no.nav.tsm.mottak.sykmelding.exceptions.SykmeldingMergeValidationException
@@ -10,7 +9,6 @@ import no.nav.tsm.mottak.sykmelding.model.InvalidRule
 import no.nav.tsm.mottak.sykmelding.model.OKRule
 import no.nav.tsm.mottak.sykmelding.model.PendingRule
 import no.nav.tsm.mottak.sykmelding.model.SykmeldingRecord
-import no.nav.tsm.mottak.sykmelding.model.TilbakedatertMerknad
 import no.nav.tsm.mottak.sykmelding.model.ValidationResult
 import no.nav.tsm.mottak.sykmelding.model.ValidationType
 import org.apache.kafka.clients.producer.KafkaProducer
@@ -26,7 +24,6 @@ class SykmeldingService(
     private val sykmeldingRepository: SykmeldingRepository,
     private val kafkaTemplate: KafkaProducer<String, SykmeldingRecord>,
     private val pdlClient: PdlClient,
-    private val manuellbehandlingService: ManuellbehandlingService,
     @Value("\${spring.kafka.topics.sykmeldinger-output}") private val tsmSykmeldingTopic: String,
 ) {
 
@@ -50,10 +47,7 @@ class SykmeldingService(
             log.warn("Sykmelding with id $sykmeldingId has differnt aktive ident for aktorId $aktorId")
         }
 
-        val newSykmeldingRecord = getManuellBehandlingRules(sykmelding)?.let {
-            log.info("Sykmelding with id $sykmeldingId has been processed manually, overriding validation with $it")
-            sykmelding.copy(validation = it)
-        } ?: getOldValidation(sykmeldingId)?.let { oldValidation ->
+        val newSykmeldingRecord = getOldValidation(sykmeldingId)?.let { oldValidation ->
             mergeSykmeldingWithOldValidation(sykmelding, oldValidation).also {
                 log.info("Sykmelding with id $sykmeldingId has old validation $oldValidation, merging with new validation: ${sykmelding.validation}, merged ${it.validation}")
             }
@@ -84,45 +78,6 @@ class SykmeldingService(
             throw SykmeldingMergeValidationException("Sykmelding rulevalidations with same timestamps but different rules ${sykmelding.sykmelding.id}")
         }
         return SykmeldingRecord(metadata, newSykmelding , mergedValidation)
-    }
-
-    fun getManuellBehandlingRules(sykmelding: SykmeldingRecord): ValidationResult? {
-        val manualDoneTimestamp = manuellbehandlingService.getManuellBehandlingTimestamp(sykmelding.sykmelding.id)
-        when (manualDoneTimestamp) {
-            null -> return null
-            else -> {
-                val pendingRule = PendingRule(
-                    name = TilbakedatertMerknad.TILBAKEDATERING_UNDER_BEHANDLING.name,
-                    timestamp = sykmelding.sykmelding.metadata.mottattDato,
-                    description = "Tilbakedatert sykmelding til manuell behandling",
-                    validationType = ValidationType.AUTOMATIC
-                )
-
-                val rules = sykmelding.validation.rules
-                if (rules.size > 1) {
-                    throw SykmeldingMergeValidationException("Sykmelding ${sykmelding.sykmelding.id}has more than one rule when it should only have one")
-                }
-
-                val newRule = if (rules.isEmpty()) {
-                    okRule(pendingRule, manualDoneTimestamp)
-                } else {
-                    when (val rule = rules.single()) {
-                        is InvalidRule -> rule.copy(timestamp = manualDoneTimestamp)
-                        is OKRule -> rule.copy(timestamp = manualDoneTimestamp)
-                        is PendingRule -> {
-                            log.warn("Sykmelding ${sykmelding.sykmelding.id} has a pending rule that is not valid maybe $rule")
-                            rule.copy(timestamp = manualDoneTimestamp)
-                        }
-                    }
-                }
-
-                return ValidationResult(
-                    status = newRule.type,
-                    timestamp = newRule.timestamp,
-                    rules = listOf(pendingRule, newRule).sortedByDescending { it.timestamp }
-                )
-            }
-        }
     }
 
     private fun okRule(pendingRule: PendingRule, manualDoneTimestamp: OffsetDateTime) = OKRule(
