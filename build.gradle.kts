bfdcd927b8cb3bbc040642ff6fcb4b403722bee4
Karl J. Overå <k@rl.run>
2025-12-02 16:42:26 +0100

    chore: bump spring patch

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "2.2.21"
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "3.5.8"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
