ae3b35b146b16dccb039a08044cd7b73641266c6
Karl J. Overå <k@rl.run>
2026-06-05 10:20:36 +0200

    chore: actually start kafka consumer
    Co-authored-by: Andreas <danduras@gmail.com>

--- src/main/kotlin/no/nav/tsm/mottak/MottakDependencies.kt
+++ src/main/kotlin/no/nav/tsm/mottak/MottakDependencies.kt
@@ -3,6 +3,7 @@ package no.nav.tsm.mottak
 import io.ktor.server.application.Application
 import io.ktor.server.plugins.di.dependencies
 import no.nav.tsm.core.dynamicDependencies
+import no.nav.tsm.mottak.db.SykmeldingRepository
 import no.nav.tsm.mottak.pdl.PdlCloudClient
 import no.nav.tsm.mottak.pdl.PdlLocalClient
 import no.nav.tsm.mottak.sykmelding.kafka.SykmeldingInputConsumer
@@ -19,6 +20,7 @@ fun Application.configureMottakDependencies() {
 
     dependencies {
         provide(SykmeldingService::class)
+        provide(SykmeldingRepository::class)
         provide(SykmeldingInputConsumer::class)
         provide(SykmeldingInputConsumerService::class)
         provide(SykmeldingProducer::class)
