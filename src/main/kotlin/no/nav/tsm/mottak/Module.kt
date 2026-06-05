aebfe9d5cb68f94189cf238896b8099bd7ac14eb
Karl J. Overå <k@rl.run>
2026-06-05 10:49:41 +0200

    fix: gracefully retry if subscribing fails

--- src/main/kotlin/no/nav/tsm/mottak/Module.kt
+++ src/main/kotlin/no/nav/tsm/mottak/Module.kt
@@ -1,15 +1,8 @@
 package no.nav.tsm.mottak
 
-import io.ktor.server.application.Application
-import io.ktor.server.application.ApplicationStarted
-import io.ktor.server.application.ApplicationStopping
-import io.ktor.server.plugins.di.dependencies
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.NonCancellable
-import kotlinx.coroutines.cancelAndJoin
+import io.ktor.server.application.*
+import io.ktor.server.plugins.di.*
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import no.nav.tsm.mottak.sykmelding.kafka.SykmeldingInputConsumerService
 
 fun Application.configureMottakModule() {
@@ -20,10 +13,5 @@ fun Application.configureMottakModule() {
 fun Application.configureConsumer() {
     val consumer: SykmeldingInputConsumerService by dependencies
 
-    var job: Job? = null
-    monitor.subscribe(ApplicationStarted) { job = launch(Dispatchers.IO) { consumer.consume() } }
-
-    monitor.subscribe(ApplicationStopping) {
-        launch { withContext(NonCancellable) { job?.cancelAndJoin() } }
-    }
+    monitor.subscribe(ApplicationStarted) { launch { consumer.consumeWithRetry() } }
 }
